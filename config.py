534ba5da64971c041795400f94028a4714d0f1f8
kirianpukhovvvvv-cell <kirianpukhovvvvv@gmail.com>
2026-03-29 21:16:37 +0600

    Update config.py

--- config.py
+++ config.py
@@ -4,3 +4,8 @@ from dotenv import load_dotenv
 load_dotenv()
 
 TOKEN = os.getenv('TELEGRAM_API_TOKEN')
+# Токен бота от @BotFather
+BOT_TOKEN = "YOUR_TELEGRAM_BOT_TOKEN_HERE"
+
+# URL API для курсов валют
+CURRENCY_API_URL = "https://api.exchangerate-api.com/v4/latest/USD"
