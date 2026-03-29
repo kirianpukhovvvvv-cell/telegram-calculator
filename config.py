c1c17cfb4cb0037ac10aaccf93ebd4049372fe81
kirianpukhovvvvv-cell <kirianpukhovvvvv@gmail.com>
2026-03-29 21:41:06 +0600

    Update config.py

--- config.py
+++ config.py
@@ -1,11 +1,14 @@
 import os
 from dotenv import load_dotenv
 
+
+# Загружаем переменные окружения из .env файла
 load_dotenv()
 
-TOKEN = os.getenv('TELEGRAM_API_TOKEN')
-# Токен бота от @BotFather
-BOT_TOKEN = "8291462397:AAE94xlijZUL45k03wfnWlzXBOKUItNB2cQ"
+
+# Токен бота от @BotFather (рекомендуется хранить в .env)
+BOT_TOKEN = os.getenv('BOT_TOKEN')
+
 
 # URL API для курсов валют
 CURRENCY_API_URL = "https://api.exchangerate-api.com/v4/latest/USD"
