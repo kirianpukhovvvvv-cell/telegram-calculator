957f40791d00990288079640cdc114473fc3d7fc
kirianpukhovvvvv-cell <kirianpukhovvvvv@gmail.com>
2026-03-29 21:54:37 +0600

    Update config.py

--- config.py
+++ config.py
@@ -1,11 +1,7 @@
 import os
 from dotenv import load_dotenv
 
-# Загружаем переменные окружения из .env файла
 load_dotenv()
 
-# Токен бота от @BotFather (рекомендуется хранить в .env)
 BOT_TOKEN = os.getenv('BOT_TOKEN')
-
-# URL API для курсов валют
 CURRENCY_API_URL = "https://api.exchangerate-api.com/v4/latest/USD"
