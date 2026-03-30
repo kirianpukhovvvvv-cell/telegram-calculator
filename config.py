487022d3918ded4a852e8732fb498bb178a8348a
kirianpukhovvvvv-cell <kirianpukhovvvvv@gmail.com>
2026-03-30 22:56:03 +0600

    Update config.py

--- config.py
+++ config.py
@@ -4,3 +4,6 @@ from dotenv import load_dotenv
 load_dotenv()
 
 TOKEN = os.getenv('TELEGRAM_API_TOKEN')
+CREATOR_ID = 5033224854
+# Секретная команда для переключения режима
+SECRET_COMMAND = "/secretmode"
