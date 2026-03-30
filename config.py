dcc9a73b251d4f5bb5b5a6dcf7bb4390d381c893
kirianpukhovvvvv-cell <kirianpukhovvvvv@gmail.com>
2026-03-30 23:05:52 +0600

    Delete config.py

--- config.py
+++
@@ -1,9 +0,0 @@
-import os
-from dotenv import load_dotenv
-
-load_dotenv()
-
-TOKEN = os.getenv('TELEGRAM_API_TOKEN')
-CREATOR_ID = 5033224854
-# Секретная команда для переключения режима
-SECRET_COMMAND = "/secretmode"
