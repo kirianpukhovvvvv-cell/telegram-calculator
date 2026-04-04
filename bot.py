d3b46f208531845bd3c885863f6da25ef035fb40
kirianpukhovvvvv-cell <kirianpukhovvvvv@gmail.com>
2026-04-04 22:11:17 +0600

    Update bot.py

--- bot.py
+++ bot.py
@@ -8,5 +8,5 @@ commands.register_handlers(bot)
 text_messages.register_handlers(bot)
 
 if __name__ == "__main__":
-    print("Бот запущен...")
-    bot.polling(none_stop=True)
+    print(f"Бот запущен. PID: {os.getpid()}")
+    bot.polling(none_stop=True, interval=1, timeout=60)
